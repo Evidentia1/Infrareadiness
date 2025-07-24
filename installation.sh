7e75a9cd487b421f7db61dba9d919a3e604a3f6b
Evidentia1 <hackathon097@gmail.com>
2025-07-24 15:03:32 +0530

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -1,9 +1,11 @@
 echo ''
 echo ' *** Startup script started'
 
+sudo su
+
 sudo apt install git -y
 sleep 3s
-git clone https://github.com/Evidentia1/ImageProcessing.git
+git clone -b master https://github.com/Evidentia1/ImageProcessing.git
 
 ls -altr
 pwd
