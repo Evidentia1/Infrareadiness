902bb4a81af755b9c427ca2c7dc572dce7336535
Evidentia1 <hackathon097@gmail.com>
2025-07-24 15:23:06 +0530

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+sudo su
+
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>log.out 2>&1
+# Everything below will go to the file 'log.out':
+
 echo ''
 echo ' *** Startup script started'
 
